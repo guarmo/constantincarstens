f5fd291260f91a78667902e4957c184d61696d70
Armando Guarino <arm.guarino@gmail.com>
2021-05-07 15:19:26 +0200

    edit contacts

--- components/main/Contacts.js
+++ components/main/Contacts.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { AiOutlineFacebook, AiOutlineInstagram } from "react-icons/ai";
+import { GrSoundcloud } from "react-icons/gr";
 
 const Contacts = () => {
   return (
-    <div>
-      <h1>Contacts</h1>
+    <div className="border-2 border-red-800 relative text-5xl flex justify-center">
+      <a
+        href="https://www.facebook.com/profile.php?id=100008687705479"
+        target="_blank"
+      >
+        <AiOutlineFacebook />
+      </a>
+      <a href="https://www.instagram.com/heduda_flinkeswiesel" target="_blank">
+        <AiOutlineInstagram />
+      </a>
+      <a href="https://soundcloud.com/hedudaflinkeswiesel" target="_blank">
+        <GrSoundcloud />
+      </a>
     </div>
   );
 };
