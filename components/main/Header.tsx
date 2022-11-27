9f4ca25c60707b0abaf4da25e230ae9841de859b
Armando Guarino <armando.guarino@berlin-bytes.de>
2022-11-27 17:50:09 +0100

    Update video component height

--- components/main/Header.tsx
+++ components/main/Header.tsx
@@ -1,18 +1,9 @@
 const Header = () => {
   return (
-    <div className="h-full">
-      <video
-        className="h-full"
-        width="100%"
-        height="100%"
-        loop={true}
-        autoPlay={true}
-        muted
-      >
-        <source src="/main-video.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+    <video loop autoPlay muted>
+      <source src="/main-video.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
   );
 };
 
