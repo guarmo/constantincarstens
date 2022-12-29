b7bfa02a40725cfe89d03d3cf2f9afa4909295b5
Armando Guarino <arm.guarino@gmail.com>
2022-12-29 17:06:47 +0100

    Add heduda assets

--- components/heduda/Links.tsx
+++ components/heduda/Links.tsx
@@ -4,7 +4,7 @@ import data from "data";
 const Nav = () => {
   return (
     <div className="">
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8 lg:gap-8">
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-12">
         {data.heduda.links.map((link, i) => (
           <Link
             className="w-full h-full"
@@ -12,7 +12,7 @@ const Nav = () => {
             href={link.href}
             target="_blank"
           >
-            <img src={link.imgSrc} alt={link.imgAlt} />
+            <img src={link.imgSrc} alt={link.imgAlt} className="min-w-full h-[370px] sm:h-[300px]" />
             <h3>{link.text}</h3>
           </Link>
         ))}
